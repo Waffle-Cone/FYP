6c8dc18d6959e9699bf1d602ea7bfd861bb9094c
Winter <ldbtci2000@gmail.com>
2023-10-17 18:30:18 +0100

    Initial commit

---
+++ Express-sprint-01/app.js
@@ -0,0 +1,53 @@
+// Imports ----------------------------------------------------------------
+import Express from "express";
+import database from "./database.js";
+
+
+// Configure express app --------------------------------------------------
+const app = new Express();
+
+// Configure middleware ---------------------------------------------------
+
+//Controllers -------------------------------------------------------------
+const boatController = async (req, res) => {
+    // Build SQL
+
+    const table = 'boats';
+    const extendedTable = `${table} LEFT JOIN models ON boats.Model_ID = models.Model_ID
+    LEFT JOIN manufacturers ON models.Manufacturer_ID = manufacturers.Manufacturer_ID
+    LEFT JOIN watercrafttypes ON models.Type_ID = watercrafttypes.Type_ID`;
+
+    const extendedField = ['boats.Registration_Number, models.Model_Name, manufacturers.Manufacturer_Name, models.Img_URL, watercrafttypes.Type, boats.Status'];
+
+    const sql = `SELECT ${extendedField} FROM ${extendedTable}`;
+ 
+    // EXECUTE SQL
+    let isSuccess = false;
+    let message = "";
+    let result = null;
+
+    try {
+        [result] = await database.query(sql);
+        if(result.length === 0) {message = "No record(s) found"}
+        else {
+        isSuccess= true;
+        message = "Record(s) found";
+        };
+    }
+    catch(e) {
+        message = `Failed to execute query: ${e.message}`;
+    };
+
+    // RESPONSES 
+    isSuccess 
+    ? res.status(200).json(result) // set status to 200 then return result as json
+    : res.status(400).json({message});// something went wrong and set status to 400 and return message as json
+  
+};
+// Endpoints --------------------------------------------------------------
+app.get('/api/boats', boatController);
+
+// Start server -----------------------------------------------------------
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
